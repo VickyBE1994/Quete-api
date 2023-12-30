8dbaede0e63c29d619580c0c730eabe8a8a263d8
vickyBE@1994 <vickyvip81@gmail.com>
2023-12-30 19:47:53 +0530

    Quete Api

---
+++ script.js
@@ -0,0 +1,16 @@
+function fetchRandomQuote(){
+    fetch ("https://api.quotable.io/random").then(response=>response.json()).then(data=>{
+       const quoteElement=document.getElementById("quote");
+       quoteElement.innerHTML=`<p style><b> CONTENT:</b><br> ${data.content}</p>
+    <p> <b>AUTHOR NAME:</b><br> ${data.author}`;
+    })
+          .catch(error => {
+            console.log("Error fetching random quote:", error);
+          });
+      }
+
+      // Call the functions to fetch data from APIs when the page loads
+    const button=document.getElementById("button").innerText="Click here"
+    document.addEventListener("click", () => {
+        fetchRandomQuote();
+    })
